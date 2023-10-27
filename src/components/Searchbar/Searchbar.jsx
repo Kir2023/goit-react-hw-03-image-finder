9d6eb06f1f1e7980cc8e6aed6bc9f2d2edac01d4
Kir2023 <132088682+Kir2023@users.noreply.github.com>
2023-10-27 20:00:21 +0300

    Start

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import css from './Searchbar.module.css'
+
+export const Searchbar =() => {
+return (
+<div className={css.search}>
+    <input
+        className="SearchInput"
+        type="text"
+        // onChange={onChange}
+        // placeholder={placeholder}
+      />
+</div>
+);
+};
